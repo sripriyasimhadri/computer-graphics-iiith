90a666e69d5073e9fa541aaabd8383aed69e04b1
sripriyasimhadri <sripriya.simahadri@gmail.com>
2019-05-20 17:16:17 +0530

    orthographic camera and perspective projections

--- SRIP/Codes/main_ortho.js
+++ SRIP/Codes/main_ortho.js
@@ -4,14 +4,16 @@ var initialize = function(){
 	    renderer.setPixelRatio( window.devicePixelRatio );
 	    renderer.setSize( window.innerWidth, window.innerHeight );
 	    document.body.appendChild(renderer.domElement);
-	    camera = new THREE.PerspectiveCamera(60,window.innerWidth / window.innerHeight,0.1,100);
-	    //let aspectRatio = window.innerWidth / window.innerHeight;
-  		//let frustumSize = 20;
-		//camera = new THREE.OrthographicCamera(frustumSize * aspectRatio / -2, frustumSize * aspectRatio / 2, frustumSize / 2, frustumSize / -2, -100, 100);
+	    //camera = new THREE.PerspectiveCamera(60,window.innerWidth / window.innerHeight,0.1,100);
+	    let aspectRatio = window.innerWidth / window.innerHeight;
+  		let frustumSize = 20;
+		camera = new THREE.OrthographicCamera(frustumSize * aspectRatio / -2, frustumSize * aspectRatio / 2, frustumSize / 2, frustumSize / -2, -100, 100);
 	    camera.position.y = 30;
 	    controls = new THREE.OrbitControls( camera );
 	    controls.damping = 0.2;
 	    controls.staticMoving = false;
+	    //var axesHelper = new THREE.AxesHelper( 5 );
+		//scene.add( axesHelper );
 	    scene = new THREE.Scene();
 	    stats = new Stats();
 	    stats.domElement.style.position = 'absolute';
@@ -19,89 +21,6 @@ var initialize = function(){
 	    stats.domElement.style.zIndex = 100;
 	    document.body.appendChild( stats.domElement );
 	    window.addEventListener( 'resize', onWindowResize, false );
-	    var material = new THREE.LineBasicMaterial({
-				color: 0x0000ff
-			});
-			var geometry = new THREE.Geometry();
-			geometry.vertices.push(
-					new THREE.Vector3( 2.8, 2, 0 ),
-					new THREE.Vector3( 10, 15, 5 ),
-					//new THREE.Vector3( 1, 0, 0 )
-			);
-
-			var line = new THREE.Line( geometry, material );
-			scene.add( line );
-
-			var material1 = new THREE.LineBasicMaterial({
-				color: 0x0000ff
-			});
-			var geometry1 = new THREE.Geometry();
-			geometry1.vertices.push(
-					new THREE.Vector3( 2.8, 2, 0 ),
-					new THREE.Vector3( -2, 15, 5 ),
-					//new THREE.Vector3( 1, 0, 0 )
-			);
-
-			var line1 = new THREE.Line( geometry1, material1 );
-			scene.add( line1 );
-
-			var material2 = new THREE.LineBasicMaterial({
-				color: 0x0000ff
-			});
-			var geometry2 = new THREE.Geometry();
-			geometry2.vertices.push(
-					new THREE.Vector3( 2.8, 2, 0 ),
-					new THREE.Vector3( -20,-8, 5 ),
-					//new THREE.Vector3( 1, 0, 0 )
-			);
-
-			var line2 = new THREE.Line( geometry2, material2 );
-			scene.add( line2 );
-
-			var material3 = new THREE.LineBasicMaterial({
-				color: 0x0000ff
-			});
-			var geometry3 = new THREE.Geometry();
-			geometry3.vertices.push(
-					new THREE.Vector3( 2.8, 2, 0 ),
-					new THREE.Vector3( 45, -8, 5 ),
-					//new THREE.Vector3( 1, 0, 0 )
-			);
-
-			var line3 = new THREE.Line( geometry3, material3 );
-			scene.add( line3 );
-
-			var material4 = new THREE.LineBasicMaterial({
-				color: 0xff0000
-			});
-			var geometry4 = new THREE.Geometry();
-			geometry4.vertices.push(
-					new THREE.Vector3( 1.8, 1.4, 0 ),
-					new THREE.Vector3( 1.8, 4.2, 0 ),
-					//new THREE.Vector3( 1, 0, 0 )
-			);
-
-			//var line4 = new THREE.Line( geometry4, material4 );
-			//scene.add( line4 );
-
-			//var material5 = new THREE.LineBasicMaterial({
-				//color: 0xff0000
-			//});
-			//var geometry5 = new THREE.Geometry();
-			//geometry5.vertices.push(
-					//new THREE.Vector3( 5, 3, 2 ),
-					//new THREE.Vector3( 5, 5, 0 ),
-					//new THREE.Vector3( 1, 0, 0 )
-			//);
-
-			//var line5 = new THREE.Line( geometry5, material5 );
-			//scene.add( line5 );
-			var geometry4 = new THREE.PlaneGeometry( 2, 2, 32 );
-			var material4 = new THREE.MeshBasicMaterial( {color: 0xffffff, wireframe: false, side: THREE.DoubleSide} );
-			var plane = new THREE.Mesh( geometry4, material4 );
-			plane.position.set(3, 2.5, 0);
-			scene.add( plane );
-
 	    var req = new XMLHttpRequest();
 	    req.open("GET","bunnyobj.txt",true);
 	    req.onload = function(){
